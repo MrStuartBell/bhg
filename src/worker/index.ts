061463b7644cb9730bef3402a6a78a421e01d7cc
MrStuartBell <stuart.bell@manumit.co.uk>
2025-06-23 16:32:40 -0400

    Update index.ts
    Updated mx records to be explicit not *.

--- src/worker/index.ts
+++ src/worker/index.ts
@@ -6,8 +6,11 @@ const app = new Hono<{ Bindings: Env }>();
 app.get("/.well-known/mta-sts.txt", (c) => {
   const policy = `version: STSv1
 mode: enforce
-mx: *.aspmx.l.google.com
-mx: *.googlemail.com
+mx: aspmx.l.google.com
+mx: alt1.aspmx.l.google.com
+mx: alt2.aspmx.l.google.com
+mx: alt3.aspmx.l.google.com
+mx: alt4.aspmx.l.google.com
 max_age: 86400`;
   
   return c.text(policy);
