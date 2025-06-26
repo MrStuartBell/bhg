e8312fe98b8e4c56c130626c326b80af36c4c528
MrStuartBell <stuart.bell@manumit.co.uk>
2025-06-26 19:42:31 -0400

    Update vite.config.ts
    Update to pick up all /public files

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { cloudflare } from "@cloudflare/vite-plugin";
-import { glob } from "glob";
-import path from "path";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
+  publicDir: 'public',
   build: {
     rollupOptions: {
-      input: Object.fromEntries(
-        glob.sync('public/*.html').map(file => [
-          // Use the filename without extension as the key
-          path.basename(file, '.html'),
-          // Use the full path as the value
-          file
-        ])
-      )
+      input: 'public/index.html'
     }
-  },
-  publicDir: 'public'
+  }
 });
