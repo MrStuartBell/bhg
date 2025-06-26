4efa7134f6e456127ac9634e3b3ae4a7570ae77e
MrStuartBell <stuart.bell@manumit.co.uk>
2025-06-26 18:21:36 -0400

    Update vite.config.ts
    reverting to prior version.

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,4 @@ import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
   plugins: [react(), cloudflare()],
-  publicDir: "./", // This tells Vite to include root files
 });
